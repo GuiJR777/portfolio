d79b3a312cdc5b43a84d3b1a0c68c4a937e629d9
Guilherme J. Ramires <guilhermejramires@gmail.com>
2020-07-06 15:42:33 -0300

    '06/07/2020'

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 function clicou(){
     document.getElementById('texto').innerHTML= 
-    'Esse projeto nasceu com objetivo de facilitar o processo de busca de informações a meu respeito, pois toda vez que me candidato a uma vaga ou ofereço um serviço a alguém, o recrutador ou o cliente, provavelmente fariam uma pesquisa a meu respeito para saber informações pessoais, profissionais e conhecer meu trabalho. Para isso eles teriam que navegar em diversas redes sociais (instagram, linkedin), analisar meu currículo e repositórios git. Essa página tráz de maneira resumida todas essas informações e disponibiliza o acesso a essas redes de maneira mais rápida, poupando assim o tempo de quem estiver me avaliando.'
+    'Esse projeto nasceu com objetivo de facilitar o processo de busca de informações a meu respeito, pois toda vez que me candidato a uma vaga ou ofereço um serviço a alguém, o recrutador ou o cliente, provavelmente faria uma pesquisa a meu respeito para saber informações pessoais, profissionais e conhecer meu trabalho. Para isso eles teriam que navegar em diversas redes sociais (instagram, linkedin), analisar meu currículo e repositórios git. Essa página traz de maneira resumida todas essas informações e disponibiliza o acesso a essas redes de maneira mais rápida, poupando assim o tempo de quem estiver me avaliando.'
 }
 function clicou2(){
     document.getElementById('texto').innerHTML= 
